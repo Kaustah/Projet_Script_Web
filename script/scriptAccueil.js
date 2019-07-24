f9eef9bdbe3eeca98300af49c5cdff1934bedba0
kaustah <kaustah.henriques@gmail.com>
2019-07-24 16:21:22 -0400

    Update cours 2019-07-24

--- script/scriptAccueil.js
+++ script/scriptAccueil.js
@@ -1,21 +1,23 @@
 document.getElementById("emplacement").addEventListener("click", function(){
+    //Ajout d'evenements pour crer 2 nouveaux elements
     if (document.querySelectorAll('input').length === 0) {
         var newInput = document.createElement("input");
         newInput.id = "freeformInput";
-        var newButton = document.createElement("button");
+        var newButton = document.createElement("input");
         newButton.type = "button";
         newButton.value = "Ajouter";
         newButton.id = "boutonAjout";
-        document.getElementById("emplacement").insertBefore(newInput,document.getElementById("emplacement").childNodes[1]);
-        document.getElementById("emplacement").insertBefore(newButton,document.getElementById("emplacement").childNodes[2]);
+        document.getElementById("texte_central").insertBefore(newInput,document.getElementById("texte_central").childNodes[7]);
+        document.getElementById("texte_central").insertBefore(newButton,document.getElementById("texte_central").childNodes[8]);
+        //Evenement: ajout d'un element <li> et suppression des 2 elements
         document.getElementById("boutonAjout").addEventListener("click", function(){
             var newListEntry = document.getElementById("freeformInput").value;
             var newList = document.createElement("li");
             newList.innerText = newListEntry;
             document.getElementById("emplacement").appendChild(newList);
             newButton.parentNode.removeChild(newButton);
-            document.getElementById("emplacement").removeChild(newInput);
-           //Lors du clique es elements disparaisse et reapparaisse
-        });
+            newInput.parentNode.removeChild(newInput);
+        });        
     }
 }); 
+
